09da9192de07a07aed80873d3e1a9c8a2e7975ee
Jemy19 <gellidojeremy@gmail.com>
2022-05-16 00:17:06 +0800

    part2 start

--- MANAGEMENT SYSTEM/client/src/components/Form/Form.js
+++ MANAGEMENT SYSTEM/client/src/components/Form/Form.js
@@ -29,7 +29,7 @@ const Form = () => {
             <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
             <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
             <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
-            <div className={classes.fileInput}><FileBase> type="file" multiple={false} onDone={({base64}) => ({ ...postData, selectedFile: base64 })} </FileBase> </div>
+            <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
             <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
             <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
