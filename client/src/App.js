9a9199b05fc84fbbe4c406e0eb543fbf8a1e8d9f
Grim <boneclinkz35@gmail.com>
2022-06-09 17:46:49 +0800

    MESSAGE MO

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import Messenger from "./pages/messenger/Messenger";
+
+function App() {
+  const { user } = useContext(AuthContext);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Messenger /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/Messenger" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/messenger">
+          {!user ? <Redirect to="/" /> : <Messenger />}
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
